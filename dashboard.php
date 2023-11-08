1aca516e6e00cae8c38117cd420181aca52c0069
Eugene Labrador <jineugenefreecs18@gmail.com>
2023-11-08 01:34:58 -0500

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="admin_styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>JCrackulon</title>
+</head>
+<body>
+    <div class="sidebar">
+        <div class="sidebar-header">
+            <h1>JCrackulon</h1>
+        </div>
+        <ul class="nav">
+            <li><a href="#home">Home</a></li>
+            <br>
+            <li><a href="#profile">Profile</a></li>
+            <br>
+            <li><a href="#view_users">View Users</a></li>
+        </ul>
+            <button class="button-85" role="button">Logout</button>
+        <div class="navigation">
+
+
+    <div class="content">
+        
+    </div>
+</body>
+</html>
